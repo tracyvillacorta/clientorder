e174be2a4c2a5b04ce10ce0bf605d650654a31d2
tracyvillacorta <tracymariellevillacorta18@gmail.com>
2022-09-17 01:00:09 +0800

    Initial commit

---
+++ clientorder.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+ $(function() {
+   $('[data-toggle="datepicker"]').datepicker({
+      autoHide: true,
+      zIndex: 2048,
+   });
+});
+
+<!-- Datepicker JS-->
+<script src="js/datepicker.js"></script>
